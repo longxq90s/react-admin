bf0f1df0897d5ee418355e35056bae8691621dec
longxq <longxq_work@163.com>
2022-10-19 17:54:37 +0800

    '解决冲突'

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ class App extends Component {
   render() {
     return (
       <div>
+<<<<<<< HEAD
         <Button type="primary"></Button>
+=======
+        <Button type="primary">点一下</Button>
+>>>>>>> main
       </div>
     );
   }
